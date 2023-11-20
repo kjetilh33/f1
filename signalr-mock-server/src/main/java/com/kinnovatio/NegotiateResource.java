14d57d40415f1b0b10b0f209e012843ac60ffa74
kjetilh33 <kjetil.halvorsen@cognite.com>
2023-11-20 20:26:31 +0100

    Add negotiate endpoint

---
+++ signalr-mock-server/src/main/java/com/kinnovatio/NegotiateResource.java
@@ -0,0 +1,34 @@
+package com.kinnovatio;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.jboss.resteasy.reactive.RestResponse;
+
+@Path("/negotiate")
+public class NegotiateResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public RestResponse<String> negotiate() {
+        return RestResponse.ResponseBuilder.ok(
+                """
+                        {
+                            "Url": "/signalr",
+                            "ConnectionToken": "blahblah",
+                            "ConnectionId": "759a9c8d-047b-4c6a-bca0-ad234b2840a5",
+                            "KeepAliveTimeout": 20.0,
+                            "DisconnectTimeout": 30.0,
+                            "ConnectionTimeout": 110.0,
+                            "TryWebSockets": true,
+                            "ProtocolVersion": "1.5",
+                            "TransportConnectTimeout": 10.0,
+                            "LongPollDelay": 1.0
+                        }
+                        """
+        )
+                .header("Set-Cookie", "my-cookie")
+                .build();
+    }
+}
