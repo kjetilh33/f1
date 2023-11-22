b5976cd3b4912a25c642fe9ba6fc010e3db2b9bf
Kjetil Halvorsen <38658408+kjetilh33@users.noreply.github.com>
2023-11-22 21:45:49 +0000

    add file reader threading

--- signalr-mock-server/src/main/java/com/kinnovatio/LiveDataFeed.java
+++ signalr-mock-server/src/main/java/com/kinnovatio/LiveDataFeed.java
@@ -1,12 +1,47 @@
 package com.kinnovatio;
 
-import jakarta.enterprise.context.SessionScoped;
-
+import java.io.BufferedReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-@SessionScoped
-public class LiveDataFeed {
+import org.jboss.logging.Logger;
+
+import jakarta.websocket.Session;
+
+public class LiveDataFeed implements Runnable {
     private static final Path practicePath = Path.of("/data/received-messages-practice3.log");
     private static final Path qualifyingPath = Path.of("/data/received-messages-qualifying.log");
     private static final Path racePath = Path.of("/data/received-messages-race.log");
+
+    private final Logger LOG = Logger.getLogger(this.getClass());
+    private final Session session;
+    private AtomicBoolean run = new AtomicBoolean(false);
+
+    LiveDataFeed(Session session) {
+        this.session = session;
+    }
+
+    public void start() {
+        run.set(true);
+        Thread.startVirtualThread(this);
+    }
+
+    public void close() {
+        run.set(false);
+    }
+
+    @Override
+    public void run() {
+        try (BufferedReader reader = Files.newBufferedReader(racePath, StandardCharsets.UTF_8)) {
+            while (run.get()) {
+                session.getAsyncRemote().sendText(reader.readLine());
+                Thread.sleep(500);
+            }
+
+        } catch (Exception e) {
+            LOG.warn("Error while reading message file: {}", e.toString(), e);
+        }        
+    }
 }
--- signalr-mock-server/src/main/java/com/kinnovatio/LiveDataWss.java
+++ signalr-mock-server/src/main/java/com/kinnovatio/LiveDataWss.java
@@ -1,12 +1,14 @@
 package com.kinnovatio;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.websocket.OnMessage;
 import jakarta.websocket.OnOpen;
 import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
 import org.jboss.logging.Logger;
-import org.jboss.logmanager.LogManager;
 
 @ServerEndpoint("/signalr/connect")
 @ApplicationScoped
@@ -15,6 +17,10 @@ public class LiveDataWss {
     private final String initMessage = """
             {"C":"adf-some-id","S":1,"M":[]}
             """;
+
+    private final String streamSubscribe = "Subscribe";
+
+    private final Map<Session, LiveDataFeed> sessions = new ConcurrentHashMap<>();
 
     @OnOpen
     public void onOpen(Session session) {
@@ -27,6 +33,9 @@ public class LiveDataWss {
 
     @OnMessage
     public void onMessage(Session session, String message) {
+        if (message.contains(streamSubscribe)) {
+            // Set up a new file stream reader
+        }
 
     }
 }
