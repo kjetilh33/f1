4146c5c4e2dc0a96319fdbd278ed7cbbf310fe1a
Kjetil Halvorsen <kjetil.halvorsen@cognite.com>
2023-12-08 18:57:02 +0100

    Fix. build script on shell

--- signalr-mock-server/build.sh
+++ signalr-mock-server/build.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+echo Building image $IMAGE
+echo Push image = $PUSH_IMAGE
 ./mvnw install -Dquarkus.container-image.build=true -Dquarkus.container-image.image=$IMAGE -Dquarkus.container-image.push=$PUSH_IMAGE
