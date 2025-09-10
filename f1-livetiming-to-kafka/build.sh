24b3a3083b2dae75dfe5639476f2e2f8edee9f02
Kjetil Halvorsen <38658408+kjetilh33@users.noreply.github.com>
2025-09-10 11:35:51 +0200

    Delete f1-livetiming-to-kafka directory

--- f1-livetiming-to-kafka/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo Building image $IMAGE
-echo Push image = $PUSH_IMAGE
-./mvnw install -Dquarkus.container-image.build=true -Dquarkus.container-image.image=$IMAGE -Dquarkus.container-image.push=$PUSH_IMAGE
