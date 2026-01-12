318de32a0c7c557173fe9fadac5c50f225ac01f4
kjetilh33 <kjetil.halvorsen@cognite.com>
2026-01-12 19:32:48 +0100

    update tests

--- f1-api/src/test/java/com/kinnovatio/F1LiveTimingWithBrokerTest.java
+++ f1-api/src/test/java/com/kinnovatio/F1LiveTimingWithBrokerTest.java
@@ -31,7 +31,8 @@ public class F1LiveTimingWithBrokerTest {
                                 {
                                 "category": "LapCount",
                                 "message": "{\"CurrentLap\":58,\"TotalLaps\":58}",
-                                "timestamp": 1766379225.760375900
+                                "timestamp": 1766379225.760375900,
+                                "isStreaming": true
                                 }
                                 """.getBytes()),
                 new ProducerRecord<>("f1-live-raw", "SessionInfo",
@@ -39,7 +40,8 @@ public class F1LiveTimingWithBrokerTest {
                                 {
                                 "category": "SessionInfo",
                                 "message": "{\"Meeting\":{\"Key\":1276,\"Name\":\"Abu Dhabi Grand Prix\",\"OfficialName\":\"FORMULA 1 ETIHAD AIRWAYS ABU DHABI GRAND PRIX 2025 \",\"Location\":\"Yas Island\",\"Number\":24,\"Country\":{\"Key\":21,\"Code\":\"UAE\",\"Name\":\"United Arab Emirates\"},\"Circuit\":{\"Key\":70,\"ShortName\":\"Yas Marina Circuit\"}},\"SessionStatus\":\"Finalised\",\"ArchiveStatus\":{\"Status\":\"Complete\"},\"Key\":9839,\"Type\":\"Race\",\"Name\":\"Race\",\"StartDate\":\"2025-12-07T17:00:00\",\"EndDate\":\"2025-12-07T19:00:00\",\"GmtOffset\":\"04:00:00\",\"Path\":\"2025/2025-12-07_Abu_Dhabi_Grand_Prix/2025-12-07_Race/\",\"_kf\":true}",
-                                "timestamp": 1766379225.760375900
+                                "timestamp": 1766379225.760375900,
+                                "isStreaming": true
                                 }
                                 """.getBytes()),
                 new ProducerRecord<>("f1-live-raw", "category",
@@ -47,7 +49,8 @@ public class F1LiveTimingWithBrokerTest {
                                 {
                                 "category": "TrackStatus",
                                 "message": "{\"Status\":\"1\",\"Message\":\"AllClear\",\"_kf\":true}",
-                                "timestamp": 1766379225.760375900
+                                "timestamp": 1766379225.760375900,
+                                "isStreaming": true
                                 }
                                 """.getBytes()));
 
