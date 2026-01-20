557bd61aa08f66255fb998725ae40dddefdab2cd
kjetilh33 <kjetil.halvorsen@cognite.com>
2026-01-20 21:49:18 +0100

    setup flowbite

--- web-ui-svelte/src/routes/+page.svelte
+++ web-ui-svelte/src/routes/+page.svelte
@@ -1,2 +1,15 @@
+<script>
+  import { Alert } from "flowbite-svelte";
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<div class="p-8">
+  <Alert>
+    <span class="font-medium">Info alert!</span>
+    Change a few things up and try submitting again.
+  </Alert>
+</div>
+
