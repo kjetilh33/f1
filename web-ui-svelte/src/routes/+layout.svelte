ee9a3110fda377166d78cd1a12ec76739b0e5b9f
Kjetil Halvorsen <38658408+kjetilh33@users.noreply.github.com>
2026-01-24 20:28:10 +0100

    Add Navbar component to layout

--- web-ui-svelte/src/routes/+layout.svelte
+++ web-ui-svelte/src/routes/+layout.svelte
@@ -2,8 +2,22 @@
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
+
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<Navbar>
+  <NavBrand href="/">
+    <!-- <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
+    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Data</span>
+  </NavBrand>
+  <NavHamburger />
+  <NavUl>
+    <NavLi href="/">Home</NavLi>
+    <NavLi href="/about">About</NavLi>
+  </NavUl>
+</Navbar>
+
 {@render children()}
