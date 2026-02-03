3243217d651a03f767737c2f7eabe1b9f88a2229
Kjetil Halvorsen <kjetil.halvorsen@cognite.com>
2026-02-03 13:00:28 +0100

    Add footer

--- web-ui-svelte/src/routes/+layout.svelte
+++ web-ui-svelte/src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import favicon from '$lib/assets/favicon.svg';
 
 	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from "flowbite-svelte";
+  import { Footer, FooterLinkGroup, FooterLink, ImagePlaceholder, TextPlaceholder, Skeleton, FooterCopyright } from "flowbite-svelte";
 
 	let { children } = $props();
 </script>
@@ -21,3 +22,13 @@
 </Navbar>
 
 {@render children()}
+
+<Footer class="absolute start-0 bottom-0 z-20 w-full border-t border-gray-200 bg-white p-4 shadow-sm md:flex md:items-center md:justify-between md:p-6 dark:border-gray-600 dark:bg-gray-800">
+  <FooterCopyright href="/" by="Kinnovatio" year={2026} />
+  <FooterLinkGroup class="mt-3 flex flex-wrap items-center text-sm text-gray-500 sm:mt-0 dark:text-gray-400">
+    <FooterLink href="/">About</FooterLink>
+    <FooterLink href="/">Privacy Policy</FooterLink>
+    <FooterLink href="/">Licensing</FooterLink>
+    <FooterLink href="/">Contact</FooterLink>
+  </FooterLinkGroup>
+</Footer>
