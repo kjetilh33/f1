250191e957d1eaefd6cf37e76fc244e1476589da
kjetilh33 <kjetil.halvorsen@cognite.com>
2026-02-16 22:18:44 +0100

    fix images

--- web-ui-svelte/src/routes/+page.svelte
+++ web-ui-svelte/src/routes/+page.svelte
@@ -1,34 +1,36 @@
 <script>
-  import { Alert } from "flowbite-svelte";
+  import { Card } from "flowbite-svelte";
   import { InfoCircleSolid } from "flowbite-svelte-icons";
 </script>
 
-<div class="mx-auto max-w-7xl px-4 py-4 sm:px-4 lg:px-4 bg-gray-300">
-  <h1>Welcome!</h1>
-  <p>...</p>
+<div class="mx-auto max-w-7xl px-4 py-4 sm:px-4 lg:px-4 lg:mt-8">
   <div class="grid grid-cols-3 gap-6">
-    <Card img="/static/2026-02_pre-season-1.jpg">
+    <Card href="/live" img="/images/2026-02_pre-season-1-720.jpg">
+      <div class="m-6">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Follow live race data</h5>
+        <p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">
+          Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+      </div>
+    </Card>
+
+    <Card img="/images/2026-02_pre-season-1-720.jpg">
       <div class="m-6">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
         <p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
       </div>
     </Card>
 
-    <Card img="/static/2026-02_pre-season-1.jpg">
+    <Card img="/images/2026-02_pre-season-1-720.jpg">
       <div class="m-6">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
         <p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
       </div>
     </Card>
-
-    <Card img="/static/2026-02_pre-season-1.jpg">
-      <div class="m-6">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-        <p class="mb-3 leading-tight font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-      </div>
-    </Card>
-
   </div>
-  <a href="https://www.vecteezy.com/free-photos/formula-1-racing">Formula 1 Racing Stock photos by Vecteezy</a>
+  <div class="flex justify-end">
+  <span class="text-xs text-gray-700 dark:text-gray-400"> <a href="https://www.vecteezy.com/free-photos/formula-1-racing">Formula 1 Racing Stock photos by Vecteezy</a></span>
 </div>
+</div>
+
+
 
