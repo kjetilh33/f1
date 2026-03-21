dcf96abce18683aa197ba1a8d7f95af08ccd2b58
Kjetil Halvorsen <kjetil.halvorsen@cognite.com>
2026-03-21 11:24:09 +0100

    Fix. Add delay to the status check scheduled job

--- f1-live-processor/src/main/java/com/kinnovatio/livetiming/processor/F1SessionInfoProcessor.java
+++ f1-live-processor/src/main/java/com/kinnovatio/livetiming/processor/F1SessionInfoProcessor.java
@@ -139,7 +139,7 @@ public class F1SessionInfoProcessor {
     /// received message. If the session is currently marked as {@code LIVE_SESSION} but no
     /// messages have been received for over 30 minutes, the session state is forcibly transitioned
     /// to {@code UNKNOWN} to reflect the potential loss of connectivity or stream termination.
-    @Scheduled(every = "2m")
+    @Scheduled(every = "2m", delayed = "30m")
     public void checkInputDataStreamAlive() {
         Duration limit = Duration.ofMinutes(30);
         Duration elapsed = Duration.between(stateManager.getLastMessageReceived(), Instant.now());
