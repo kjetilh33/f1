4577975ab2fadff8bbd83c644ed4f03156e98595
kjetilh33 <kjetil.halvorsen@cognite.com>
2026-03-23 21:45:41 +0100

    fix. make jackson case insensitive

--- f1-api/src/main/java/com/kinnovatio/f1/config/JacksonConfig.java
+++ f1-api/src/main/java/com/kinnovatio/f1/config/JacksonConfig.java
@@ -1,5 +1,6 @@
 package com.kinnovatio.f1.config;
 
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import io.quarkus.jackson.ObjectMapperCustomizer;
@@ -10,7 +11,8 @@ public class JacksonConfig implements ObjectMapperCustomizer {
 
     @Override
     public void customize(ObjectMapper objectMapper) {
-        objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.UPPER_CAMEL_CASE);
+        objectMapper.enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES);
+        //objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.UPPER_CAMEL_CASE);
     }
 
 }
