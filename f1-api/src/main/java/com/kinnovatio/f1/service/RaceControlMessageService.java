aa3703cddfac359a4d2d12021f9949955a226b38
Kjetil Halvorsen <kjetil.halvorsen@cognite.com>
2026-03-28 13:18:07 +0100

    fix. json serialization of the race cotrol message

--- f1-api/src/main/java/com/kinnovatio/f1/service/RaceControlMessageService.java
+++ f1-api/src/main/java/com/kinnovatio/f1/service/RaceControlMessageService.java
@@ -1,19 +1,16 @@
 package com.kinnovatio.f1.service;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.kinnovatio.f1.model.SessionInfoRaw;
 import com.kinnovatio.f1.model.SessionMessage;
-import com.kinnovatio.f1.model.SessionStatus;
 import com.kinnovatio.f1.repository.RaceControlMessagesRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.jboss.logging.Logger;
 
 import java.util.List;
-import java.util.Optional;
 
 @ApplicationScoped
 public class RaceControlMessageService {
@@ -30,7 +27,14 @@ public class RaceControlMessageService {
         ObjectNode root = objectMapper.createObjectNode();
         ArrayNode messages = root.putArray("messages");
         for (SessionMessage message : raceControlMessages) {
-            messages.add(objectMapper.valueToTree(message.message()));
+            try {
+                messages.add(objectMapper.readTree(message.message()));
+            } catch (JsonProcessingException e) {
+                LOG.warnf("Error parsing the raw race control message into a Json tree structure. Error: %s",
+                        e.getMessage());
+                throw new RuntimeException(e);
+            }
+
         }
 
         return root;
