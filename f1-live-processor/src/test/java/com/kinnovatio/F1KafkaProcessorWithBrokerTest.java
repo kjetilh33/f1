ec2d6c861c3486d2f6ceeb47dd2fe446fed670a6
Kjetil Halvorsen <kjetil.halvorsen@cognite.com>
2026-05-07 16:01:19 +0200

    Fix. migrate to instant

--- f1-live-processor/src/test/java/com/kinnovatio/F1KafkaProcessorWithBrokerTest.java
+++ f1-live-processor/src/test/java/com/kinnovatio/F1KafkaProcessorWithBrokerTest.java
@@ -40,19 +40,19 @@ public class F1KafkaProcessorWithBrokerTest {
                 new ProducerRecord<>("test-f1-live-raw", "LapCount",
                         objectMapper.writeValueAsString(
                                 new LiveTimingMessage("LapCount", "{\"CurrentLap\":58,\"TotalLaps\":58}",
-                                        Instant.now().atZone(ZoneId.of("UTC")), true)
+                                        Instant.now(), true)
                         )
                 ),
                 new ProducerRecord<>("test-f1-live-raw", "SessionInfo",
                         objectMapper.writeValueAsString(
                                 new LiveTimingMessage("SessionInfo", "{\"Meeting\":{\"Key\":1276,\"Name\":\"Abu Dhabi Grand Prix\",\"OfficialName\":\"FORMULA 1 ETIHAD AIRWAYS ABU DHABI GRAND PRIX 2025 \",\"Location\":\"Yas Island\",\"Number\":24,\"Country\":{\"Key\":21,\"Code\":\"UAE\",\"Name\":\"United Arab Emirates\"},\"Circuit\":{\"Key\":70,\"ShortName\":\"Yas Marina Circuit\"}},\"SessionStatus\":\"Finalised\",\"ArchiveStatus\":{\"Status\":\"Complete\"},\"Key\":9839,\"Type\":\"Race\",\"Name\":\"Race\",\"StartDate\":\"2025-12-07T17:00:00\",\"EndDate\":\"2025-12-07T19:00:00\",\"GmtOffset\":\"04:00:00\",\"Path\":\"2025/2025-12-07_Abu_Dhabi_Grand_Prix/2025-12-07_Race/\",\"_kf\":true}",
-                                        Instant.now().atZone(ZoneId.of("UTC")), true)
+                                        Instant.now(), true)
                         )
                 ),
                 new ProducerRecord<>("test-f1-live-raw", "TrackStatus",
                         objectMapper.writeValueAsString(
                                 new LiveTimingMessage("TrackStatus", "{\"Status\":\"1\",\"Message\":\"AllClear\",\"_kf\":true}",
-                                        Instant.now().atZone(ZoneId.of("UTC")), true)
+                                        Instant.now(), true)
                         )
                 )
         );
