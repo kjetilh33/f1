70916be4202fd3961aa020117f9dd3ba75d87fc5
Kjetil Halvorsen <kjetil.halvorsen@cognite.com>
2026-06-05 13:39:01 +0200

    Remove unused method

--- signalr-core-to-kafka/src/test/java/com/kinnovatio/signalr/MessageDecoderIntegrationTest.java
+++ signalr-core-to-kafka/src/test/java/com/kinnovatio/signalr/MessageDecoderIntegrationTest.java
@@ -1,7 +1,5 @@
 package com.kinnovatio.signalr;
 
-import java.util.List;
-
 import com.kinnovatio.signalr.messages.MessageDecoder;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -9,18 +7,6 @@ import org.slf4j.LoggerFactory;
 
 public class MessageDecoderIntegrationTest {
     final Logger LOG = LoggerFactory.getLogger(this.getClass());
-
-    @Test
-    void messageEncoderTest() throws Exception {
-        final String hub = "streaming";
-        final String method = "subscribe";
-        final List<Object> arguments = List.of(List.of("Heartbeat", "CarData.z"));
-        final int identifier = 1;
-
-        String json = MessageDecoder.toMessageJson(hub, method, arguments, identifier);
-        LOG.info("Returned json: \n {}", json);
-
-    }
 
     @Test
     void unzipTest() throws Exception {
