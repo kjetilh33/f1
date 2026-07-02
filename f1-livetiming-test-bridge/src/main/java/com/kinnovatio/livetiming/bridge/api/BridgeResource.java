b8ad6c85fe94ecf1d9672e374eb131bc8d7a06e5
Kjetil Halvorsen <kjetil.halvorsen@cognite.com>
2026-07-02 19:43:21 +0200

    change base rest api path

--- f1-livetiming-test-bridge/src/main/java/com/kinnovatio/livetiming/bridge/api/BridgeResource.java
+++ f1-livetiming-test-bridge/src/main/java/com/kinnovatio/livetiming/bridge/api/BridgeResource.java
@@ -10,14 +10,12 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-import jakarta.ws.rs.core.Response;
 import org.jboss.logging.Logger;
-
-import java.util.Map;
 
 @ApplicationScoped
 @RunOnVirtualThread
 @Produces(MediaType.APPLICATION_JSON)
+@Path("api")
 public class BridgeResource {
     private static final Logger LOG = Logger.getLogger(BridgeResource.class);
 
