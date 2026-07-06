69f713bbbbd011d4529f5ad7734370de89b5e402
kjetilh33 <kjetil.halvorsen@cognite.com>
2026-07-06 22:37:19 +0200

    finish dictionary

--- web-ui-svelte/src/lib/dictionaries.js
+++ web-ui-svelte/src/lib/dictionaries.js
@@ -27,7 +27,57 @@ export const TIMING_STATUS_DICTIONARY = {
     },
     8: {
         label: "Unknown",
+        color: "Grey",
+        comment: "Unknown"
+    },
+    16: {
+        label: "Pit lane",
+        color: "DarkGrey",
+        comment: "Went through this mini sector in the pit lane."
+    },
+    32: {
+        label: "Pit exit",
+        color: "DarkGrey",
+        comment: "Recently exited the pit lane."
+    },
+    64: {
+        label: "Racing",
+        color: "Black",
+        comment: "Indicates 'normal' in races. Most common status. Only turned off when the driver has retired."
+    },
+    128: {
+        label: "Unknown",
+        color: "Grey",
+        comment: "Unknown"
+    },
+    256: {
+        label: "Unknown",
+        color: "Grey",
+        comment: "Unknown"
+    },
+    512: {
+        label: "Out lap",
         color: "DarkGrey",
         comment: "Unknown"
+    },
+    1024: {
+        label: "Checkered flag",
+        color: "DarkBlue",
+        comment: "The driver passes the checkered flag."
+    },
+    2048: {
+        label: "Segment completed",
+        color: "Gold",
+        comment: "Segment completed. If this is the only status, then a yellow segment."
+    },
+    4096: {
+        label: "Overtook",
+        color: "DarkGreen",
+        comment: "The driver has recently overtaken another driver."
+    },
+    8192: {
+        label: "Overtaken",
+        color: "DarkRed",
+        comment: "The driver has recently been overtaken by another driver."
     }
 }
